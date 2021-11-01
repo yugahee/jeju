190be443fb93670e745a035485ab9fa207c899fe
Jeongonhwa <onhwav4432@gmail.com>
2021-11-01 20:31:07 +0900

    Room class 잘못 만든 거 삭제

--- src/reservation/controller/Room_reservation.java
+++ src/reservation/controller/Room_reservation.java
@@ -1,11 +1,16 @@
 package reservation.controller;
 
 import java.io.IOException;
+import java.util.List;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import host.model.vo.Rooms;
+import reservation.model.service.ReservationService;
 
 /**
  * Servlet implementation class Room_reservation
@@ -27,7 +32,7 @@ public class Room_reservation extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		
-		
+		List<Rooms> roomList = new ReservationService().selectRoom();
 		
 		
 		request.getRequestDispatcher("/views/reservation/room_reservation.jsp").forward(request, response);
