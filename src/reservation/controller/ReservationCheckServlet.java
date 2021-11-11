15d59947dde31344ca5a2b4221078a7c3dce22ed
유가희 <ygh9040@naver.com>
2021-11-12 01:48:26 +0900

    주석 삭제

--- src/reservation/controller/ReservationCheckServlet.java
+++ src/reservation/controller/ReservationCheckServlet.java
@@ -44,8 +44,7 @@ public class ReservationCheckServlet extends HttpServlet {
 		userId = ((Member)request.getSession().getAttribute("loginUser")).getUser_id();
 		
 		// 예약리스트 가져오기
-		List<Reservation> reservationList = new ReservationService().userReservation(userId);
-		System.out.println(reservationList);
+		List<Reservation> reservationList = new ReservationService().userReservation(userId);		
 		
 		Date current = new Date();
 		
