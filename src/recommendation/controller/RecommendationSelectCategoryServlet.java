a1810a40033528e9790b089e83eeebc6c6f7b5d9
유가희 <ygh9040@naver.com>
2021-11-14 15:40:26 +0900

    추천장소 수정

--- src/recommendation/controller/RecommendationSelectCategoryServlet.java
+++ src/recommendation/controller/RecommendationSelectCategoryServlet.java
@@ -41,50 +41,10 @@ public class RecommendationSelectCategoryServlet extends HttpServlet {
 		String recoKeyword = request.getParameter("recoKeyword");
 		String radio1 = request.getParameter("radio1");
 		
-		
 		recoList = new RecoService().selectList(recoArea, recoCategory, recoKeyword, radio1);
 		
-/*		if(request.getParameter("recoArea") != null) {		// 지역 선택
-			int recoArea = Integer.parseInt(request.getParameter("recoArea"));
-			
-			if(request.getParameter("recoCategory") != null) {		// 카테고리 선택
-				int recoCategory = Integer.parseInt(request.getParameter("recoCategory"));
-				recoList = new RecoService().selectList(recoArea, recoCategory);
-				
-				if(request.getParameter("radio1") != null) {	// 라디오버튼 선택
-					int radio1 = Integer.parseInt(request.getParameter("radio1"));
-					
-					recoList = new RecoService().selectNewList(recoArea, recoCategory, radio1);
-				}
-			} else {
-				recoList = new RecoService().selectList(recoArea);				
-			}
-		} else {
-			if(request.getParameter("recoCategory") != null) {	 	// 지역 선택되지 않았는데 카테고리 선택된 경우
-				int recoCategory = Integer.parseInt(request.getParameter("recoCategory"));
-				recoList = new RecoService().selectList(recoCategory);
-				
-				if(request.getParameter("radio1") != null) {		// 지역 선택되지 않음 && 카테고리 선택 && 라디오버튼 선택
-					int radio1 = Integer.parseInt(request.getParameter("radio1"));
-					
-					recoList = new RecoService().selectList(recoCategory, radio1);
-				}
-			}
-			recoList = new RecoService().selectList();
-		}*/
-		
-//		if(request.getParameter("recoArea") != null && request.getParameter("recoCategory") != null) {		// 지역과 카테고리 모두 설정된 경우
-//			int recoArea = Integer.parseInt(request.getParameter("recoArea"));
-//			int recoCategory = Integer.parseInt(request.getParameter("recoCategory"));
-//			int radio1 = Integer.parseInt(request.getParameter("radio1"));
-//			recoList = new RecoService().selectList(recoArea, recoCategory);			
-//		} else {		// 지역과 카테고리 모두 설정되지 않은 경우
-//			recoList = new RecoService().selectList();
-//		}
-		
+
 		request.setAttribute("recoList", recoList);
-		
-		System.out.println(recoList);
 		
 		request.getRequestDispatcher("/views/recommendation/reco_main.jsp").forward(request, response);
 	}
